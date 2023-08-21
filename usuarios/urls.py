60a19601bd7e077aadd0431d43e2e969c6ee8ee6
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-08-21 16:29:41 -0300

    finalização do django auth

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.models import User
 
 urlpatterns = [
      path('cadastro/', views.cadastro, name='cadastro'),
-     path('login/', views.login, name="login")
+     path('login/', views.login, name="login"),
+     path('plataforma/', views.plataforma, name='plataforma')
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as Login_django
+from django.contrib.auth.decorators import login_required
 
 def cadastro(request):
     if request.method == 'GET':
@@ -9,10 +13,31 @@ def cadastro(request):
         email = request.POST.get('email')
         senha = request.POST.get('senha')
 
-        user = User.objects.get(username=username)
+        user = User.objects.filter(username=username).first()  # Filtra usuários por username
+
         if user:
             return HttpResponse('Já existe um usuário com esse username')
-        return HttpResponse(username)
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+
+        return HttpResponse(f'Usuário {username} cadastrado com sucesso!')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+        if user:
+            Login_django(request, user)
+            return HttpResponse('autenticado')
+        else:
+            return HttpResponse('E-mail ou senha inválidos')
+
+
+@login_required(login_url='/auth/login/')
+def plataforma(request):
+    return HttpResponse('Plataforma')
